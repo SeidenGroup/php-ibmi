61b66cd0b7afe0f0ebb382a8fe96286e93b1e1ad
Calvin Buckley <calvin@cmpct.info>
2021-06-21 10:18:40 -0400

    Copyright headers in code

--- php_ibmi_int.h
+++ php_ibmi_int.h
@@ -1,3 +1,19 @@
+/*
+   +----------------------------------------------------------------------+
+   | Copyright (c) Seiden Group                                           |
+   +----------------------------------------------------------------------+
+   | This source file is subject to version 3.01 of the PHP license,      |
+   | that is bundled with this package in the file LICENSE, and is        |
+   | available through the world-wide-web at the following url:           |
+   | https://www.php.net/license/3_01.txt                                 |
+   | If you did not receive a copy of the PHP license and are unable to   |
+   | obtain it through the world-wide-web, please send a note to          |
+   | license@php.net so we can mail you a copy immediately.               |
+   +----------------------------------------------------------------------+
+   | Author: Calvin Buckley <calvin@cmpct.info>                           |
+   +----------------------------------------------------------------------+
+ */
+
 /* Internal header for sharing imports, since php_ibmi.h can be public */
 #ifdef HAVE_CONFIG_H
 # include "config.h"
--- systemcl.c
+++ systemcl.c
@@ -1,4 +1,26 @@
-/* ibmi extension for PHP (c) 2021 Calvin Buckley */
+/*
+   +----------------------------------------------------------------------+
+   | Copyright (c) The PHP Group                                          |
+   | Copyright (c) Seiden Group                                           |
+   +----------------------------------------------------------------------+
+   | This source file is subject to version 3.01 of the PHP license,      |
+   | that is bundled with this package in the file LICENSE, and is        |
+   | available through the world-wide-web at the following url:           |
+   | https://www.php.net/license/3_01.txt                                 |
+   | If you did not receive a copy of the PHP license and are unable to   |
+   | obtain it through the world-wide-web, please send a note to          |
+   | license@php.net so we can mail you a copy immediately.               |
+   +----------------------------------------------------------------------+
+   | Author: Rasmus Lerdorf <rasmus@php.net>                              |
+   |         Ilia Alshanetsky <iliaa@php.net>                             |
+   |         Calvin Buckley <calvin@cmpct.info>                           |
+   +----------------------------------------------------------------------+
+ */
+
+/*
+ * This file is based on ext/standard/exec.c from PHP, and retains its
+ * copyright and authorship notices.
+ */
 #include "php_ibmi_int.h"
 
 #include <stdio.h>
